c9600d592ab76e557cbae2260e1f8434d5de2c38
박준우(Junwoo Park)/BotService개발팀/SKP <skp.1003874@sk.com>
2019-02-21 16:55:25 +0900

    fix resize

--- data_loader.py
+++ data_loader.py
@@ -25,7 +25,7 @@ class ImTextDataset(Dataset):
         self.image_dir = os.path.join(self.data_dir, dataset, 'images')
         self.data_path = os.path.join(data_dir, dataset, 'train/data.h5py')
         #self.data = h5py.File(os.path.join(data_dir, dataset, 'train/data.h5py'), 'r')['train']
-        self.trans_img = transforms.Compose([transforms.Resize(image_size), #transforms.CenterCrop(image_size),
+        self.trans_img = transforms.Compose([transforms.Resize((image_size, image_size)), #transforms.CenterCrop(image_size),
                                              transforms.ToTensor(),])# transformation for output image
         self.cap_size_per_img = cap_size_per_img
 
